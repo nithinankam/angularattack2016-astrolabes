f3cc68dd18ff40ef042bef2a117985eba91b75e0
Sanket Tajane <sanket.tajane@gmail.com>
2016-05-15 00:56:01 -0500

    feat(add-repos): add more repos to the app

--- app/repos.service.ts
+++ app/repos.service.ts
@@ -4,15 +4,42 @@ import {Injectable} from '@angular/core'
 @Injectable()
 export class ReposService {
     private repos = [
-        {owner: "sindresorhus", repo: "awesome", name: "MainRepo"},
-        {owner: "gianarb", repo: "awesome-angularjs", name: "Angular"},
+        {owner: "diegocard", repo: "awesome-html5", name: "HTML5"},
+        {owner: "sotayamashita", repo: "awesome-css", name: "CSS"},
+        {owner: "AngularClass", repo: "awesome-angular2", name: "JavaScript"},
+        {owner: "aleksandar-todorovic", repo: "awesome-c", name: "C"},
+        {owner: "fffaraz", repo: "awesome-cpp", name: "C/C++"},
+        {owner: "akullpp", repo: "awesome-java", name: "Java"},
+        {owner: "markets", repo: "awesome-ruby", name: "Ruby"},
+        {owner: "dzharii", repo: "awesome-typescript", name: "TypeScript"},
+        {owner: "peterkokot", repo: "awesome-jquery", name: "JQuery"},
+        {owner: "vinta", repo: "awesome-python", name: "Python"},
+        {owner: "lauris", repo: "awesome-scala", name: "Scala"},
+        {owner: "kdabir", repo: "awesome-groovy", name: "Groovy"},
         {owner: "AngularClass", repo: "awesome-angular2", name: "Angular2"},
+        {owner: "gianarb", repo: "awesome-angularjs", name: "Angular"},
+        {owner: "enaqx", repo: "awesome-react", name: "React"},
         {owner: "sadcitizen", repo: "awesome-backbone", name: "Backbone"},
-        {owner: "dnbard", repo: "awesome-knockout", name: "Knockout"},
         {owner: "Urigo", repo: "awesome-meteor", name: "Meteor"},
         {owner: "nmec", repo: "awesome-ember", name: "Ember"},
-        {owner: "behzad888", repo: "awesome-aurelia", name: "Aurelia"},
-        {owner: "lauris", repo: "awesome-scala", name: "Scala"}
+        {owner: "wasabeef", repo: "awesome-android-ui", name: "Android-UI"},
+        {owner: "cjwirth", repo: "awesome-ios-ui", name: "iOS UI"},
+        {owner: "candelibas", repo: "awesome-ionic2", name: "Ionic2"},
+        {owner: "willianjusten", repo: "awesome-svg", name: "SVG"},
+        {owner: "raphamorim", repo: "awesome-canvas", name: "Canvas"},
+        {owner: "wbkd", repo: "awesome-d3", name: "D3"},
+        {owner: "veggiemonk", repo: "awesome-docker", name: "Docker"},
+        {owner: "ekremkaraca", repo: "awesome-rails", name: "Rails"},
+        {owner: "fcambus", repo: "nginx-resources", name: "nginx"},
+        {owner: "shlomi-noach", repo: "awesome-mysql", name: "MySQL"},
+        {owner: "ramnes", repo: "awesome-mongodb", name: "MongoDB"},
+        {owner: "d3viant0ne", repo: "awesome-rethinkdb", name: "RethinkDB"},
+        {owner: "onurakpolat", repo: "awesome-bigdata", name: "BigData"},
+        {owner: "youngwookim", repo: "awesome-hadoop", name: "Hadoop"},
+        {owner: "tayllan", repo: "awesome-algorithms", name: "Algorithms"},
+        {owner: "christian-bromann", repo: "awesome-selenium", name: "Selenium"},
+        {owner: "melvin0008", repo: "awesome-projects-boilerplates", name: "Boilerplates"},
+        {owner: "HQarroum", repo: "awesome-iot", name: "IOT"}
     ];
 
     getRepos() {
